5d38ae4db655068d5c2c3644074a9861b417f868
rlaghf555 <rlaghf555@naver.com>
2020-06-16 18:57:24 +0900

    movement Change, simple ui

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,38 +1,108 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private NavMeshAgent pathfinder;
-    private Vector3 target;
-    private Camera camera;
+    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
+    public float rotateSpeed = 180f; // 좌우 회전 속도
+    private NavMeshAgent agent;
+    private Animator playerAnimator;
+    private PlayerInput playerInput;
+    private Rigidbody playerRigidbody;
+    public Transform objectPivot;
+    public Transform leftHandMount;
+    public Transform RightHandMount;
+    private bool dig;
     // Start is called before the first frame update
     void Start()
     {
-        pathfinder = GetComponent<NavMeshAgent>();
-        camera = FindObjectOfType<Camera>();
-        target = new Vector3();
+        agent = GetComponent<NavMeshAgent>();
+        playerAnimator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        dig = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))  // 마우스가 클릭 되면
+        /*
+        if (Input.GetMouseButtonUp(0))
         {
-            Debug.Log("마우스 입력");
-            Vector3 mos = Input.mousePosition;
-            mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
 
-            Vector3 dir = camera.ScreenToWorldPoint(mos);
-            // 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-            NavMeshHit nhit;
-            NavMesh.SamplePosition(dir, out nhit, 10, NavMesh.AllAreas);
-            pathfinder.destination = nhit.position;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                transform.LookAt(hit.point);
+                agent.SetDestination(hit.point);
+                playerAnimator.SetBool("Walking", true);
+                agent.isStopped = false;
+            }
+
+
         }
-        
+
+        if (agent.remainingDistance <= 0.2f && agent.velocity.magnitude >= 0.2f)
+        {
+            playerAnimator.SetBool("Walking", false);
+        }
+        */
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnimator.SetTrigger("Digging");
+            dig = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Rotate();
+        Move();
+        playerAnimator.SetFloat("Move", playerInput.move);
+
+    }
+    // 입력값에 따라 캐릭터를 앞뒤로 움직임
+    private void Move()
+    {
+        // 상대적으로 이동할 거리 계산
+        Vector3 moveDistance =
+            playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        // 리지드바디를 통해 게임 오브젝트 위치 변경
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+    }
+
+    // 입력값에 따라 캐릭터를 좌우로 회전
+    private void Rotate()
+    {
+        // 상대적으로 회전할 수치 계산
+        float turn =
+            playerInput.rotate * rotateSpeed * Time.deltaTime;
+        // 리지드바디를 통해 게임 오브젝트 회전 변경
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (!dig)
+            return;
+        objectPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+      playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+      playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+      
+      playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+      playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+       //playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+       //playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+       //
+       //playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, RightHandMount.position);
+       //playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, RightHandMount.rotation);
+
+
     }
 }
